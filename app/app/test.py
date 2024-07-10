fa8ddfb22a18db463ba172992426ce8827311fd1
Locheir <mohitguptadp7@gmail.com>
2024-07-09 23:25:05 -0700

    updated code

--- app/app/test.py
+++ app/app/test.py
@@ -5,20 +5,19 @@
 from django.test import SimpleTestCase
 from app import calc
 
-class CalcTests(SimpleTestCase) :
+class CalcTests(SimpleTestCase):
     """ Test the calc method """
 
     def test_add_numbers(self) :
         """ Test adding numbers together """
 
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
-    def test_sub_numbers(self) :
+    def test_sub_numbers(self):
         """ Test subtracting numbers """
 
-        res = calc.sub(10,15)
+        res = calc.sub(10, 15)
 
-        self.assertEqual(res,5)
-        
+        self.assertEqual(res, 5)
