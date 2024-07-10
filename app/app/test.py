c12dea4be6190f063e792fb3bd536dbfa9ea0712
Locheir <mohitguptadp7@gmail.com>
2024-07-09 23:30:33 -0700

    updated code

--- app/app/test.py
+++ app/app/test.py
@@ -1,11 +1,13 @@
-"""
-    Sample tests
+"""Sample tests
 """
 
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
+
+
     """ Test the calc method """
 
     def test_add_numbers(self):
