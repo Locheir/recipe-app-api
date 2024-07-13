7aa01ec72ce48d734b085f296f046a6dea55c518
Locheir <mohitguptadp7@gmail.com>
2024-07-13 11:27:24 +0530

    keyword error

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -11,6 +11,7 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import (
     Recipe,
     Tag,
+    Ingredient,
 )
 from recipe import serializers
 
@@ -42,7 +43,8 @@ class TagViewSet(
     mixins.DestroyModelMixin,
     mixins.UpdateModelMixin,
     mixins.ListModelMixin,
-    viewsets.GenericViewSet):
+    viewsets.GenericViewSet
+):
 
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
@@ -52,4 +54,16 @@ class TagViewSet(
 
     def get_queryset(self):
         """Filter queryset to authenticate users."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage ingredients in the database."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
